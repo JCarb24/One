91fcafdc015ebf6ba13a114798f457e75a215650
JCarb <jesuscar1997@yahoo.com>
2021-12-13 21:18:50 -0500

    my first rust program :)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, Cringelord :)!");
+
+    let a: u8 = 1;
+    let b = a.to_string();
+    let c= my_awesome_vec(b);
+    
+    println!("Hello, Cringelord {}, {}!", a , c[0]);
+}
+
+fn my_awesome_vec(c:String)->Vec<String>{
+    let mut b = Vec::new();
+    b.push(c);
+    b
 }
